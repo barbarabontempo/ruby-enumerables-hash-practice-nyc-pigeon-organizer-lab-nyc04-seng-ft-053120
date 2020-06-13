72e530f6e51c633badf9e591a6761fbba3cc5fd1
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-13 17:26:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,8 @@ def nyc_pigeon_organizer(data)
         final_array[name] = {}
         end
         if !final_array[name][key]
-          !final_array[name][key]
+          !final_array[name][key] = []
+          
         end
       end
     end
