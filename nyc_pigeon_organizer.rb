aa74b61760f7dd4cdb88901bb9b62b9295b1d070
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-13 17:27:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, value), final_array|
+  final_results = data.each_with_object({}) do |(key, value), final_array|
   #binding.pry
     value.each do |inner_keys, names|
       names.each do |name|
@@ -15,6 +15,6 @@ def nyc_pigeon_organizer(data)
         final_array[name][key].push(inner_keys)
       end
     end
-    final_array
   end
+  binding.pry
 end
