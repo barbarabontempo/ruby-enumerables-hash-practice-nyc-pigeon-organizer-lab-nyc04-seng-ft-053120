1169e6deaf2a932155aa14a45a26125ab2537e85
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-13 17:27:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
   #binding.pry
     value.each do |inner_keys, names|
       names.each do |name|
-      binding.pry
+      #binding.pry
         if !final_array[name]
         final_array[name] = {}
         end
